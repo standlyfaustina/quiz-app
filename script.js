843ded9afa401190eda39b6a4313e91381a66705
Mr Standly <standlyfaustina@gmail.com>
2021-05-20 22:11:44 +0100

    data in js

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is javascript',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    },{
+        question: 'What is the most used progamming language in 2020',
+        a: 'java',
+        b: 'c',
+        c: 'javacript',
+        d: 'python',
+        correct: 'c',
+    },{
+        question: 'Who is the President of US',
+        a: 'Barrack Obama',
+        b: 'Donal Trump' ,
+        c: 'Ivan Salvado',
+        d: 'Hilary Cliton',
+        correct: 'b',
+    },{
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Highly made language',
+        c: 'Jason Object Notation',
+        d: 'Application programming Interface',
+        correct: 'a',
+    },{
+        question: 'What year was Javascript Lauch',
+        a: '1995',
+        b: '2019',
+        c: '2005',
+        d: 'none of the above',
+        correct: 'a'
+    }
+]
